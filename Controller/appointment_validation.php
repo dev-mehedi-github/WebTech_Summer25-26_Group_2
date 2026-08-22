14c61a01772f72aed2731399dca2f2a037500940
Md. Mushfiqur Rahman <mushfiqur.2102@gmail.com>
2026-08-22 20:12:08 +0600

    Edited
    Remove unnecessary comment in appointment validation.

--- Controller/appointment_validation.php
+++ Controller/appointment_validation.php
@@ -1,6 +1,4 @@
 <?php
-// Used by view_appointments.php - read-only, no appointment editing needed
-
 $appointmentList = json_decode(file_get_contents("../Model/appointment_demo.json"), true);
 if (!is_array($appointmentList)) {
     $appointmentList = [];
