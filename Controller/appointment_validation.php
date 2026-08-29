8f036af22fa55d8835eb01c1c0f2860e99277b6e
mushfiq-2102 <mushfiqur.2102@gmail.com>
2026-08-29 16:58:57 +0600

    Update appointment validation

--- Controller/appointment_validation.php
+++ Controller/appointment_validation.php
@@ -1,6 +1,9 @@
 <?php
-$appointmentList = json_decode(file_get_contents("../Model/appointment_demo.json"), true);
-if (!is_array($appointmentList)) {
-    $appointmentList = [];
-}
+require_once '../Model/db_connect.php';
+
+$appointmentList = $pdo->query(
+    "SELECT id, patient_name, doctor_name, appointment_date AS date, appointment_time AS time, status
+     FROM appointments
+     ORDER BY appointment_date DESC, id DESC"
+)->fetchAll();
 ?>
